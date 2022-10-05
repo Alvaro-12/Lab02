45e853cb2e264b4f2685d002829faa5fb1cfbe40
Alvaro Lopez <lt0895032020@unab.edu.sv>
2022-10-04 18:32:59 -0600

    finish

--- app/src/main/java/com/example/laboratorio02/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio02/MainActivity.kt
@@ -1,54 +1,58 @@
 package com.example.laboratorio02
 
-import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var edit_Nombre: EditText
-    lateinit var edit_Edad: EditText
-    lateinit var edit_departamento: EditText
-    var MOSTRAR: ListView?=null
-    var Nombre: TextView?=null
+    lateinit var Nombre: EditText
+    lateinit var Edad: EditText
+    lateinit var departamento: EditText
+    lateinit var Lv_Lista: ListView
+    lateinit var Lv_Datos: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edit_Nombre = findViewById(R.id.edit_Nombre)
-        edit_Edad = findViewById(R.id.edit_Edad)
-        edit_departamento = findViewById(R.id.edit_departamento)
-        MOSTRAR = findViewById(R.id.MOSTRAR)
-        Nombre = findViewById(R.id.Nombre)
+
+        Nombre = findViewById(R.id.edit_Nombre)
+        Edad = findViewById(R.id.edit_Edad)
+        departamento = findViewById(R.id.edit_departamento)
+        Lv_Lista = findViewById(R.id.Lv_Lista)
+        Lv_Datos = findViewById(R.id.Lv_Datos)
 
     }
     fun registrar(Vista: View)
     {
+        //Nombre.add(edit_Nombre)
         val Reg = arrayListOf<String>()
-        val nombre =edit_Nombre?.text.toString()
+        val nombre =Nombre?.text.toString()
         Reg.add(nombre)
         println(Reg)
 
         val Reg1 = arrayListOf<String>()
-        val edad = edit_Edad?.text.toString().toInt()
+        val edad = Edad?.text.toString().toInt()
         Reg1.add(edad.toString())
         println(Reg1)
 
         val Reg2 = arrayListOf<String>()
-        val departamento = edit_departamento.toString()
+        val departamento = departamento?.text.toString()
         Reg2.add(departamento)
         println(Reg2)
 
-        var adaptador:ArrayAdapter<String> = ArrayAdapter <String>(this,R.layout.files)
-        MOSTRAR?.adapter=adaptador
 
-        
+        var adaptador: ArrayAdapter<String> = ArrayAdapter <String>(this,R.layout.files,Reg)
+        Lv_Lista?.adapter=adaptador
+
+        Lv_Lista?.onItemClickListener=object : AdapterView.OnItemClickListener
+        {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Lv_Datos.setText("La Persona ${Reg.get(p2)} de edad ${Reg1.get(p2)} años, vive en el departamento ${Reg2.get(p2)}")
+
+            }
 
         }
-
-
-
-
+        }
 }
